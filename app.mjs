11f21ec4b3e277d5af60a86a08ca0e792f6850df
Eisss20 <pajenan.j@gmail.com>
2024-11-09 19:53:33 +0700

    Feat : Add setting versel to test deploy

--- app.mjs
+++ app.mjs
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/profiles", (req, res) => {
   return res.json({
@@ -11,6 +15,7 @@ app.get("/profiles", (req, res) => {
     },
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
